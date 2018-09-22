8a363e4e8a15941da78d85681c5515b8daae82b1
Chengziqian <18663104463@163.com>
2018-09-22 14:43:13 +0800

    time range judge for vote

--- router/Vote/id.js
+++ router/Vote/id.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const CheckLogined = require('../../middleware/CheckLogined');
 const validate = require('../../libs/validate');
 const DB = require('../../models');
+const moment = require('moment');
 let router = new Router();
 
 router.get('/:id/result', async (ctx, next) => {
@@ -30,31 +31,34 @@ router.post('/:id', CheckLogined, async (ctx, next) => {
   return next()
 }, async (ctx, next) => {
   let vote = await DB.Vote.findById(ctx.params.id);
-  console.count(vote);
   if (!vote) {
-    ctx.throw(422, {inputError: {vote: ['比赛不存在']}})
+    ctx.throw(422, {inputError: {vote: ['比赛不存在']}});
   } else {
-    if (ctx.request.body.options.length !== vote.count) {
-      ctx.throw(422, {inputError: {options: ['输入不合法']}})
+    if (!(moment().isBetween(moment(vote.start_time), moment(vote.end_time)))) {
+      ctx.throw(400, '投票未开始或已过期');
     } else {
-      if (ctx.USER.hasHasVote(vote)) ctx.throw(402, '您已投票');
-      else {
-        await (async () => {
-          let t;
-          try {
-            t = await DB.sequelize.transaction();
-            for (let i = 0; i < ctx.request.body.options.length; i++) {
-              let o = await DB.Option.findById(+ctx.request.body.options[i], {transaction: t});
-              await o.update({count: (+o.count) + 1}, {transaction: t});
+      if (ctx.request.body.options.length !== vote.count) {
+        ctx.throw(422, {inputError: {options: ['输入不合法']}})
+      } else {
+        if (await ctx.USER.hasHasVote(vote)) ctx.throw(402, '您已投票');
+        else {
+          await (async () => {
+            let t;
+            try {
+              t = await DB.sequelize.transaction();
+              for (let i = 0; i < ctx.request.body.options.length; i++) {
+                let o = await DB.Option.findById(+ctx.request.body.options[i], {transaction: t});
+                await o.update({count: (+o.count) + 1}, {transaction: t});
+              }
+              await ctx.USER.setHasVotes([vote]);
+              await t.commit();
+              ctx.response.status = 200;
+            } catch (e) {
+              await t.rollback();
+              ctx.throw(e);
             }
-            await ctx.USER.setHasVotes([vote]);
-            await t.commit();
-            ctx.response.status = 200;
-          } catch (e) {
-            await t.rollback();
-            ctx.throw(e);
-          }
-        })()
+          })()
+        }
       }
     }
   }
